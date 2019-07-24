26b6fb2ad5a55a197a5e152b3208a246845efc41
eko <febri1412@gmail.com>
2019-07-24 09:16:16 +0700

    navigate mountain

--- src/components/MountainFlatlist.js
+++ src/components/MountainFlatlist.js
@@ -14,9 +14,12 @@ class Mountainlist extends Component {
             <View style={styles.itemMount}>
                 <View style={styles.itemMount}>
                     <View style={styles.listStyle}>
-                    <Text style={styles.statusText}>
+                        <TouchableOpacity onPress={()=> this.props.navigation.navigate('MountainDetail')}>
+                        <Text style={styles.statusText}>
                         {item.status}
                     </Text>
+                        </TouchableOpacity>
+                    
                     <Text style={styles.mountTitle}>{"Gunung "+item.name}</Text>
                     <Text style={styles.mountDetail}>{"Tinggi : "+item.tinggi}</Text>
                     <Text style={styles.mountDetail}>{"Level : "+item.level}</Text>
